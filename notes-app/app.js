7a810fa852841ca1914369bef258c03251760c84
Paulo Cardoso <pcardoso.lei@gmail.com>
2022-03-06 21:21:13 +0000

    finishing section 3

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,11 @@
-const getNotes = require('./notes.js')
+const getNotes = require('./notes.js');
+const chalk = require('chalk');
 
+
+console.log(chalk.green.inverse.bold('Success!'))
 console.log(getNotes());
+
+
 
 // const add = require('./utils.js')
 // const sum = add(4, -2)
